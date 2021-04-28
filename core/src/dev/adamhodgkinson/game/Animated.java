9a02a2bfe8c76c089fb3ac76dcfe658decfa80dd
noodleWrecker7 <hodgkinson.adam@gmail.com>
2021-04-28 12:51:18 +0100

    animation tings

--- core/src/dev/adamhodgkinson/game/Animated.java
+++ core/src/dev/adamhodgkinson/game/Animated.java
@@ -9,7 +9,26 @@ public abstract class Animated {
 
     float animTime = 0f;
     HashMap<String, Animation<TextureRegion>> animations = new HashMap<>();
+    String currentAnimation = "idle"; // default
 
+    void addAnimation(String name, Animation<TextureRegion> animation) {
+        animations.put(name, animation);
+    }
+
+    void playAnimation(String name) {
+        animTime = 0f;
+        if (animations.containsKey(name)) {
+            currentAnimation = name;
+        }
+    }
+
+    Animation<TextureRegion> getAnimation() {
+        return animations.get(currentAnimation);
+    }
+
+    public TextureRegion getFrame() {
+        return animations.get(currentAnimation).getKeyFrame(animTime);
+    }
 
 
     public void update(float dt) {
--- core/src/dev/adamhodgkinson/game/Player.java
+++ core/src/dev/adamhodgkinson/game/Player.java
@@ -6,12 +6,8 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.*;
 
-public class Player {
+public class Player extends Animated {
     Body body;
-    Animation<TextureRegion> runAnimation;
-    Animation<TextureRegion> hitAnimation;
-    Animation<TextureRegion> idleAnimation;
-    float animTime = 0;
 
     public Player(World world, AssetManager assets) {
         BodyDef def = new BodyDef();
@@ -29,12 +25,15 @@ public class Player {
 
         // needs to dynamically figure out asset name
         // todo make interface Animated for animations to be managed by
-        runAnimation = new Animation<TextureRegion>(.25f, ((TextureAtlas) assets.get("packed/pack.atlas")).findRegions("game/sprites/elf_m_run_anim"), Animation.PlayMode.LOOP);
-        hitAnimation = new Animation<TextureRegion>(.5f, ((TextureAtlas) assets.get("packed/pack.atlas")).findRegions("game/sprites/elf_m_hit_anim"), Animation.PlayMode.NORMAL);
-        idleAnimation = new Animation<TextureRegion>(.25f, ((TextureAtlas) assets.get("packed/pack.atlas")).findRegions("game/sprites/elf_m_idle_anim"), Animation.PlayMode.LOOP);
+        Animation<TextureRegion> runAnimation = new Animation<TextureRegion>(.25f, ((TextureAtlas) assets.get("packed/pack.atlas")).findRegions("game/sprites/elf_m_run_anim"), Animation.PlayMode.LOOP);
+        Animation<TextureRegion> hitAnimation = new Animation<TextureRegion>(.5f, ((TextureAtlas) assets.get("packed/pack.atlas")).findRegions("game/sprites/elf_m_hit_anim"), Animation.PlayMode.NORMAL);
+        Animation<TextureRegion> idleAnimation = new Animation<TextureRegion>(.25f, ((TextureAtlas) assets.get("packed/pack.atlas")).findRegions("game/sprites/elf_m_idle_anim"), Animation.PlayMode.LOOP);
+        this.addAnimation("idle", idleAnimation);
+        this.addAnimation("run", runAnimation);
+        this.addAnimation("hit", hitAnimation);
     }
 
-    public TextureRegion getFrame() { // figures out which animation should be used and gets the frame
+   /* public TextureRegion getFrame() { // figures out which animation should be used and gets the frame
 //        return idleAnimation.getKeyFrame(animTime);
         if (isHitPlaying) {
             return hitAnimation.getKeyFrame(animTime);
@@ -42,26 +41,19 @@ public class Player {
             return idleAnimation.getKeyFrame(animTime);
         }
 
-    }
+    }*/
 
-    boolean isHitPlaying = false, isRunning = false, isIdle = true;
-
-    public void getHit() { // called when hit and resets animation settings
-        animTime = 0;
-        isHitPlaying = true;
-        isRunning = false;
-        isIdle = false;
-    }
-
-    public boolean isHitPlaying() {
-        return isHitPlaying;
-    }
-
+    boolean isRunning = false;
+    public void getHit(){playAnimation("hit");}
+    @Override
     public void update(float dt) { // keeps track of time for animations
         animTime += dt;
-        if (isHitPlaying && hitAnimation.isAnimationFinished(animTime)) {
-            isHitPlaying = false;
-            isIdle = true;
+        if(currentAnimation.equals("hit") && getAnimation().isAnimationFinished(animTime)){
+            if(isRunning) {
+                playAnimation("run");
+            } else {
+                playAnimation("idle");
+            }
         }
     }
 }
