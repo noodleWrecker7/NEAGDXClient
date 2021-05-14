390f5ae79b77206185b16df4787567eb23a698ff
noodlewrecker7 <hodgkinson.adam@gmail.com>
2021-05-14 15:26:36 +0100

    tilemap building

--- core/src/dev/adamhodgkinson/game/TileGroup.java
+++ core/src/dev/adamhodgkinson/game/TileGroup.java
@@ -5,43 +5,53 @@ import com.badlogic.gdx.physics.box2d.*;
 
 import java.util.ArrayList;
 
-public class TileGroup implements Physical{
+public class TileGroup implements Physical {
     GameBodyType type;
     private ArrayList<Tile> tiles;
     Body body;
     Fixture fixture;
-
+    World world;
     ChainShape chainShape;
 
     public ArrayList<Tile> getTiles() {
         return tiles;
     }
 
-    public TileGroup(World world) {
+    public TileGroup(World _world) {
         tiles = new ArrayList<>();
+        world = _world;
 
+        createBody();
+
+    }
+
+    public void setBodyType(GameBodyType type) {
+        this.type = type;
+    }
+
+
+    public void createBody() {
+        if (body != null) {
+            world.destroyBody(body);
+        }
         BodyDef def = new BodyDef();
         def.position.set(0, 0);
         body = world.createBody(def);
 
         body.setUserData(this);
+        fixtures = new ArrayList<>();
     }
 
-    public void setBodyType(GameBodyType type){
-        this.type = type;
 
-    }
+    ArrayList<Fixture> fixtures = new ArrayList<>();
 
     protected void build() {
-        if (fixture != null) {
-            body.destroyFixture(fixture);
-        }
-        ArrayList<Vector2> vectors = new ArrayList<>();
+        createBody();
         TileMapShapeBuilder builder = new TileMapShapeBuilder();
         for (int i = 0; i < tiles.size(); i++) {
             Tile t = tiles.get(i);
             builder.addTile(t);
-            Vector2 v1 = new Vector2(t.getX() - .5f, t.getY() - .5f); // i could definitely have looped this bit
+     /*       Vector2 v1 = new Vector2(t.getX() - .5f, t.getY() - .5f); // i could definitely have looped this bit
             Vector2 v2 = new Vector2(t.getX() - .5f, t.getY() + .5f);
             Vector2 v3 = new Vector2(t.getX() + .5f, t.getY() - .5f);
             Vector2 v4 = new Vector2(t.getX() + .5f, t.getY() + .5f);
@@ -49,17 +59,19 @@ public class TileGroup implements Physical{
             vectors.add(v1);
             vectors.add(v2);
             vectors.add(v3);
-            vectors.add(v4);
+            vectors.add(v4);*/
         }
+        fixtures = builder.build(body);
 
-        Vector2[] array = new Vector2[vectors.size()];
-        vectors.toArray(array);
-        chainShape = new ChainShape();
+      /*  Vector2[] array = new Vector2[vectors.size()];
+        vectors.toArray(array);*/
+        /*chainShape = new ChainShape();
         chainShape.createChain(array);
-        fixture = body.createFixture(chainShape, 1);
+        fixture = body.createFixture(chainShape, 1);*/
 
-        System.out.println("og vertices" + array.length);
-        System.out.println("chain verts " + chainShape.getVertexCount());
+        System.out.println("Built");
+//        System.out.println("og vertices" + array.length);
+//        System.out.println("chain verts " + chainShape.getVertexCount());
 
 
     }
--- core/src/dev/adamhodgkinson/game/TileMapShapeBuilder.java
+++ core/src/dev/adamhodgkinson/game/TileMapShapeBuilder.java
@@ -1,66 +1,132 @@
 package dev.adamhodgkinson.game;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.ChainShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class TileMapShapeBuilder {
 
     ArrayList<Edge> edges = new ArrayList<>();
 
-    public TileMapShapeBuilder(){
+    public TileMapShapeBuilder() {
 
     }
 
-    public void addTile(Tile t){
+    public void addTile(Tile t) {
         Vector2 v1 = new Vector2(t.getX() - .5f, t.getY() - .5f);
         Vector2 v2 = new Vector2(t.getX() - .5f, t.getY() + .5f);
         Vector2 v3 = new Vector2(t.getX() + .5f, t.getY() + .5f);
         Vector2 v4 = new Vector2(t.getX() + .5f, t.getY() - .5f);
 
-        edges.add(new Edge(v1,v2));
-        edges.add(new Edge(v2,v3));
-        edges.add(new Edge(v3,v4));
-        edges.add(new Edge(v4,v1));
+        edges.add(new Edge(v1, v2));
+        edges.add(new Edge(v2, v3));
+        edges.add(new Edge(v3, v4));
+        edges.add(new Edge(v4, v1));
     }
 
-    public void build(){
+    public ArrayList<Fixture> build(Body b) {
+        System.out.println("Deleting edges");
         deleteCommonEdges();
         deleteCollinearEdges();
+        ArrayList<ArrayList<Edge>> fixtures = separateToFixtures();
+
+        return createFixtures(b, fixtures);
+
     }
 
-    public void deleteCommonEdges(){
+    public ArrayList<Fixture> createFixtures(Body b, ArrayList<ArrayList<Edge>> fixtures) {
+        ArrayList<Fixture> physicsFixtures = new ArrayList<>();
+        for (int i = 0; i < fixtures.size(); i++) {
+            ArrayList<Edge> fixture = fixtures.get(i);
+            ArrayList<Vector2> vectors = new ArrayList<>();
+            for (int j = 0; j < fixture.size(); j++) {
+                vectors.add(fixture.get(j).p1);
+            }
+            ChainShape chainShape = new ChainShape();
+            chainShape.createChain((Vector2[]) vectors.toArray());
+            physicsFixtures.add(b.createFixture(chainShape, 1));
+        }
+
+        return physicsFixtures;
+    }
+
+
+    public void deleteCommonEdges() {
         for (int i = 0; i < edges.size(); i++) {
             for (int j = 0; j < edges.size(); j++) {
-                if(i==j) continue;
+                if (i == j) continue;
 
-                if(edges.get(i).equals(edges.get(j))) {
+                if (edges.get(i).equals(edges.get(j))) {
                     ArrayList<Integer> r = new ArrayList<>();
                     r.add(i);
                     r.add(j);
                     edges.removeAll(r);
                     i -= 2;
+                    if(i<0) i=0;
+                    if(j<0) j=0;
                 }
             }
         }
     }
 
-    public void deleteCollinearEdges(){
+    public void deleteCollinearEdges() {
         for (int i = 0; i < edges.size(); i++) {
             for (int j = 0; j < edges.size(); j++) {
                 Edge e1 = edges.get(i);
                 Edge e2 = edges.get(j);
+                if (e1.p2.equals(e2.p1)) {
+                    edges.set(i, new Edge(e1.p1, e2.p2));
+                    edges.remove(j);
+                    i--;
+                }
             }
         }
     }
+
+    public ArrayList<ArrayList<Edge>> separateToFixtures() {
+        ArrayList<ArrayList<Edge>> fixtures = new ArrayList<>();
+
+        ArrayList<Edge> fixture = new ArrayList<>();
+        fixture.add(edges.remove(0));
+        while (edges.size() > 0) {
+            int nextIndex = indexOfNextEdge(fixture.get(fixture.size() - 1).p2);
+            if (nextIndex == -1) {
+                fixtures.add(fixture);
+                fixture = new ArrayList<>();
+                fixture.add(edges.remove(0));
+            }
+            Edge nextEdge = edges.get(nextIndex);
+            fixture.add(nextEdge);
+            edges.remove(nextEdge);
+
+        }
+
+        return fixtures;
+    }
+
+
+    public int indexOfNextEdge(Vector2 p2) {
+        for (int i = 0; i < edges.size(); i++) {
+            if (edges.get(i).p1.equals(p2)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
 
-class Edge{
+class Edge {
     public Vector2 p1;
     public Vector2 p2;
-    public Edge(float x1,float y1,float x2,float y2){
-        this(new Vector2(x1,y1), new Vector2(x2,y2));
+
+    public Edge(float x1, float y1, float x2, float y2) {
+        this(new Vector2(x1, y1), new Vector2(x2, y2));
     }
+
     public Edge(Vector2 _p1, Vector2 _p2) {
         this.p1 = _p1;
         this.p2 = _p2;
@@ -69,6 +135,6 @@ class Edge{
     public boolean equals(Edge edge) {
         if (this == edge) return true;
         if (edge == null || getClass() != edge.getClass()) return false;
-        return p1.equals(edge.p1) && p2.equals(p2) || p1.equals(edge.p2) && p2.equals(p1) ;
+        return p1.equals(edge.p1) && p2.equals(p2) || p1.equals(edge.p2) && p2.equals(p1);
     }
 }
