2082caa55135cafe5b45d9103535d08579c5392d
noodleWrecker7 <adam@adamhodgkinson.dev>
2021-08-22 19:33:26 +0100

    not really any business logic

--- core/src/dev/adamhodgkinson/game/Player.java
+++ core/src/dev/adamhodgkinson/game/Player.java
@@ -8,35 +8,38 @@ import com.badlogic.gdx.physics.box2d.World;
 
 public class Player extends Sprite implements Physical {
 
-    GameBodyType type = GameBodyType.PLAYER;
+	GameBodyType type = GameBodyType.PLAYER;
 
-    public Player(World world, AssetManager assets, float x, float y) {
-        super(world, x, y, "elf_m", assets);
+	public Player(World world, AssetManager assets, float x, float y) {
+		super(world, x, y, "elf_m", assets);
 
-        this.speed = 8f;
-        float width = 2;
-        float height = 3;
-        float sensorHeight = .4f;
+		speed = 8f;
+		final float width = 2;
+		final float height = 3;
+		final float sensorHeight = .4f;
 
-        // sensor
-        FixtureDef sensorFixtureDef = new FixtureDef();
-        PolygonShape shape2 = new PolygonShape();
-        shape2.setAsBox(width / 2, sensorHeight / 2, new Vector2(0, -height / 2), 0);
-        sensorFixtureDef.isSensor = true;
-        sensorFixtureDef.shape = shape2;
-        body.createFixture(sensorFixtureDef);
+		// sensor
+		final FixtureDef sensorFixtureDef = new FixtureDef();
+		final PolygonShape shape2 = new PolygonShape();
+		shape2.setAsBox(width / 2, sensorHeight / 2, new Vector2(0, -height / 2), 0);
+		sensorFixtureDef.isSensor = true;
+		sensorFixtureDef.shape = shape2;
+		body.createFixture(sensorFixtureDef);
 
-        body.setUserData(this);
-    }
+		body.setUserData(this);
+	}
 
-
-    /**Takes a 2d direction vector and adds it to the current movement vector, this is only called once every time
-     * a key is pressed or released so the maximum total range for the movement vector is -1 to +1
-     * @param x the x direction
-     * @param y the y direction*/
-    public void addMoveDir(int x, int y) {
-        movement.add(x, y*2);
-        updateFlippage();
-    }
+	/**
+	 * Takes a 2d direction vector and adds it to the current movement vector, this
+	 * is only called once every time a key is pressed or released so the maximum
+	 * total range for the movement vector is -1 to +1
+	 *
+	 * @param x the x direction
+	 * @param y the y direction
+	 */
+	public void addMoveDir(int x, int y) {
+		movement.add(x, y * 2);
+		updateFlippage();
+	}
 
 }
--- core/src/dev/adamhodgkinson/game/navigation/NavGraph.java
+++ core/src/dev/adamhodgkinson/game/navigation/NavGraph.java
@@ -4,99 +4,97 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class NavGraph {
-    Vertex[] nodesArray;
-    ArrayList<Vertex> initalNodes = new ArrayList();
-    /**
-     * To save time accessing finding a nodes index
-     */
-    final int[][] coordToIndexMap;
-    Arc[][] adjacencyMatrix;
-    boolean compiled = false;
+	Vertex[] nodesArray;
 
-    public Arc[][] getAdjacencyMatrix() {
-        return adjacencyMatrix;
-    }
+	ArrayList<Vertex> initalNodes = new ArrayList();
+	/**
+	 * To save time accessing finding a nodes index
+	 */
+	final int[][] coordToIndexMap;
+	Arc[][] adjacencyMatrix;
+	boolean compiled = false;
 
-    public NavGraph(int mapWidth, int mapHeight) {
-        coordToIndexMap = new int[mapWidth][mapHeight];
-        // fills with default, invalid values
-        for (int i = 0; i < mapWidth; i++) {
-            Arrays.fill(coordToIndexMap[i], -1);
-        }
+	public Arc[][] getAdjacencyMatrix() {
+		return adjacencyMatrix;
+	}
 
-    }
+	public NavGraph(int mapWidth, int mapHeight) {
+		coordToIndexMap = new int[mapWidth][mapHeight];
+		// fills with default, invalid values
+		for (int i = 0; i < mapWidth; i++) {
+			Arrays.fill(coordToIndexMap[i], -1);
+		}
 
-    public int addVertex(short x, short y) {
-        return addVertex(new Vertex(x, y));
-    }
+	}
 
-    public Vertex[] getNodesArray() {
-        return nodesArray;
-    }
+	public int addVertex(short x, short y) {
+		return addVertex(new Vertex(x, y));
+	}
 
-    /**
-     * Registers a vertex to the graph, does nothing if compile() has already been called
-     *
-     * @return index of node
-     */
-    public int addVertex(Vertex v) {
-        if (compiled) {
-            return -1;
-        }
-        initalNodes.add(v);
-        int index = initalNodes.size() - 1;
-        if (index == Integer.MAX_VALUE - 1) { // shouldn't ever actually be able to happen
-            return -1;
-        }
-        coordToIndexMap[v.x][v.y] = index;
-        System.out.println(index);
-        return index;
-    }
+	public Vertex[] getNodesArray() {
+		return nodesArray;
+	}
 
-    /**
-     * Finalizes node list and creates empty adjacency matrix
-     */
-    public void compile() {
-        nodesArray = new Vertex[initalNodes.size()];
-        initalNodes.toArray(nodesArray);
-        initalNodes.clear();
-        initalNodes = null;
-        adjacencyMatrix = new Arc[nodesArray.length][nodesArray.length];
-        System.out.println("adj matrix");
-    }
+	/**
+	 * Registers a vertex to the graph, does nothing if compile() has already been
+	 * called
+	 *
+	 * @return index of node
+	 */
+	public int addVertex(Vertex v) {
+		if (compiled)
+			return -1;
+		initalNodes.add(v);
+		final int index = initalNodes.size() - 1;
+		if (index == Integer.MAX_VALUE - 1)
+			return -1;
+		coordToIndexMap[v.x][v.y] = index;
+		System.out.println(index);
+		return index;
+	}
 
-    /**
-     * Directional from 1 to 2
-     */
-    public Arc addLinearEdge(short x1, short y1, short x2, short y2, byte weight) {
-        int index1 = coordToIndexMap[x1][y1];
-        int index2 = coordToIndexMap[x2][y2];
-        adjacencyMatrix[index1][index2] = new Arc(weight);
-        return adjacencyMatrix[index1][index2];
-    }
+	/**
+	 * Finalizes node list and creates empty adjacency matrix
+	 */
+	public void compile() {
+		nodesArray = new Vertex[initalNodes.size()];
+		initalNodes.toArray(nodesArray);
+		initalNodes.clear();
+		initalNodes = null;
+		adjacencyMatrix = new Arc[nodesArray.length][nodesArray.length];
+		System.out.println("adj matrix");
+	}
 
-    public JumpArc addJumpEdge(short x1, short y1, short x2, short y2, byte weight, double xSpeed, double jumpSpeed) {
-        int index1 = coordToIndexMap[x1][y1];
-        int index2 = coordToIndexMap[x2][y2];
-        JumpArc arc = new JumpArc(weight, xSpeed, jumpSpeed);
-        adjacencyMatrix[index1][index2] = arc;
-        return arc;
-    }
+	/**
+	 * Directional from 1 to 2
+	 */
+	public Arc addLinearEdge(short x1, short y1, short x2, short y2, byte weight) {
+		final int index1 = coordToIndexMap[x1][y1];
+		final int index2 = coordToIndexMap[x2][y2];
+		adjacencyMatrix[index1][index2] = new Arc(weight);
+		return adjacencyMatrix[index1][index2];
+	}
 
-    public void addBiDirEdge(short x1, short y1, short x2, short y2, byte weight, boolean isJump) {
-        addLinearEdge(x1, y1, x2, y2, weight);
-        addLinearEdge(x2, y2, x1, y1, weight);
-    }
+	public JumpArc addJumpEdge(short x1, short y1, short x2, short y2, byte weight, double xSpeed, double jumpSpeed) {
+		final int index1 = coordToIndexMap[x1][y1];
+		final int index2 = coordToIndexMap[x2][y2];
+		final JumpArc arc = new JumpArc(weight, xSpeed, jumpSpeed);
+		adjacencyMatrix[index1][index2] = arc;
+		return arc;
+	}
 
-    public Vertex getVertexByCoords(short x, short y) {
-        if (x < 0 || y < 0) {
-            return null;
-        }
-        int index = coordToIndexMap[x][y];
-        if (index < 0) {
-            return null;
-        }
-        return nodesArray[index];
+	public void addBiDirEdge(short x1, short y1, short x2, short y2, byte weight, boolean isJump) {
+		addLinearEdge(x1, y1, x2, y2, weight);
+		addLinearEdge(x2, y2, x1, y1, weight);
+	}
 
-    }
+	public Vertex getVertexByCoords(short x, short y) {
+		if (x < 0 || y < 0)
+			return null;
+		final int index = coordToIndexMap[x][y];
+		if (index < 0)
+			return null;
+		return nodesArray[index];
+
+	}
 }
