5dd2490a66b79a6047dba227475bf5c58e8c107f
noodleWrecker7 <adam@adamhodgkinson.dev>
2021-09-20 11:26:23 +0100

    little finite state machine for player states and minor adjustments to pathfinding

--- core/src/dev/adamhodgkinson/game/Player.java
+++ core/src/dev/adamhodgkinson/game/Player.java
@@ -6,9 +6,12 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
+import dev.adamhodgkinson.game.UserInputHandler.Action;
+
 public class Player extends Sprite implements Physical {
 
 	GameBodyType type = GameBodyType.PLAYER;
+	State _state = State.IDLE;
 
 	public Player(World world, AssetManager assets, float x, float y) {
 		super(world, x, y, "elf_m", assets);
@@ -29,6 +32,49 @@ public class Player extends Sprite implements Physical {
 		body.setUserData(this);
 	}
 
+	public void handleInput(Action input) {
+		switch (_state) {
+		case RUNNING:
+			if (input == Action.JUMP) {
+				jump();
+			}
+			handleLeftRightMovement(input);
+			break;
+		case JUMPING:
+			handleLeftRightMovement(input);
+			_state = State.JUMPING;
+			break;
+		case IDLE:
+			if (input == Action.JUMP) {
+				jump();
+			}
+			handleLeftRightMovement(input);
+			break;
+		}
+	}
+
+	/** If there is any left right movement it is acted upon */
+	public void handleLeftRightMovement(Action input) {
+		switch (input) {
+		case MOVE_LEFT_END:
+			addMoveDir(1, 0);
+			break;
+		case MOVE_LEFT_START:
+			addMoveDir(-1, 0);
+			break;
+		case MOVE_RIGHT_END:
+			addMoveDir(-1, 0);
+			break;
+		case MOVE_RIGHT_START:
+			addMoveDir(1, 0);
+			break;
+		}
+	}
+
+	public void jump() {
+		body.setLinearVelocity(body.getLinearVelocity().x, jumpSpeed);
+	}
+
 	/**
 	 * Takes a 2d direction vector and adds it to the current movement vector, this
 	 * is only called once every time a key is pressed or released so the maximum
@@ -39,7 +85,16 @@ public class Player extends Sprite implements Physical {
 	 */
 	public void addMoveDir(int x, int y) {
 		movement.add(x, y * 2);
+		if (movement.x == 0) {
+			_state = State.IDLE;
+		} else {
+			_state = State.RUNNING;
+		}
 		updateFlippage();
 	}
 
+}
+
+enum State {
+	JUMPING, RUNNING, IDLE
 }
--- core/src/dev/adamhodgkinson/game/Sprite.java
+++ core/src/dev/adamhodgkinson/game/Sprite.java
@@ -6,113 +6,128 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class Sprite extends Animated implements Physical {
-    Vector2 movement = new Vector2();
-    Body body;
-    float speed;
-    boolean isRunning = false;
-    float width;
-    float height;
+	Vector2 movement = new Vector2();
+	Body body;
+	float speed;
+	float jumpSpeed;
+	boolean isRunning = false;
+	float width;
+	float height;
 
-    public Sprite(World world, float x, float y, String textureName, AssetManager assets) {
-        this(world, x, y, 5, 8f, 4f, 0, textureName, assets);
-    }
+	public Sprite(World world, float x, float y, String textureName, AssetManager assets) {
+		this(world, x, y, 5, 8f, 4f, 0, textureName, assets);
+	}
 
-    public Sprite(World world, float x, float y, float density, float speed, float friction, float linearDamping, String textureName, AssetManager assets) {
-        this.speed = speed;
-        TextureAtlas atlas = assets.get("packed/pack.atlas");
-        String prefix = "game/sprites/" + textureName;
-        width = (float) atlas.findRegions(prefix + "_idle_anim").get(0).packedWidth / 16;
-        height = (float) atlas.findRegions(prefix + "_idle_anim").get(0).packedHeight / 16;
-        setDefaultAnims(assets, textureName);
+	public Sprite(World world, float x, float y, float density, float speed, float friction, float linearDamping,
+			String textureName, AssetManager assets) {
+		this.speed = speed;
+		jumpSpeed = speed * 2;
+		final TextureAtlas atlas = assets.get("packed/pack.atlas");
+		final String prefix = "game/sprites/" + textureName;
+		width = (float) atlas.findRegions(prefix + "_idle_anim").get(0).packedWidth / 16;
+		height = (float) atlas.findRegions(prefix + "_idle_anim").get(0).packedHeight / 16;
+		setDefaultAnims(assets, textureName);
 
-        BodyDef def = new BodyDef();
-        def.position.set(x, y);
-        def.type = BodyDef.BodyType.DynamicBody;
-        body = world.createBody(def);
+		final BodyDef def = new BodyDef();
+		def.position.set(x, y);
+		def.type = BodyDef.BodyType.DynamicBody;
+		body = world.createBody(def);
 
-        body.setFixedRotation(true);
+		body.setFixedRotation(true);
 
-        // main fixture
-        FixtureDef fix = new FixtureDef();
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox(width / 2, (height / 2)); // height/8 accounts for whitespace in texture
-        fix.shape = shape;
-        fix.density = density;
-        fix.friction = friction;
-        body.setLinearDamping(linearDamping);
-        body.createFixture(fix);
-    }
+		// main fixture
+		final FixtureDef fix = new FixtureDef();
+		final PolygonShape shape = new PolygonShape();
+		shape.setAsBox(width / 2, height / 2); // height/8 accounts for whitespace in texture
+		fix.shape = shape;
+		fix.density = density;
+		fix.friction = friction;
+		body.setLinearDamping(linearDamping);
+		body.createFixture(fix);
+	}
 
-    public void setDefaultAnims(AssetManager assets, String textureName) {
-        TextureAtlas atlas = assets.get("packed/pack.atlas");
-        String prefix = "game/sprites/" + textureName;
-        Animation<TextureRegion> runAnimation = new Animation<TextureRegion>(.15f, atlas.findRegions(prefix + "_run_anim"), Animation.PlayMode.LOOP);
-        Animation<TextureRegion> hitAnimation = new Animation<TextureRegion>(.5f, atlas.findRegions(prefix + "_hit_anim"), Animation.PlayMode.NORMAL);
-        Animation<TextureRegion> idleAnimation = new Animation<TextureRegion>(.25f, atlas.findRegions(prefix + "_idle_anim"), Animation.PlayMode.LOOP);
-        this.addAnimation("idle", idleAnimation);
-        this.addAnimation("run", runAnimation);
-        this.addAnimation("hit", hitAnimation);
+	public void setDefaultAnims(AssetManager assets, String textureName) {
+		final TextureAtlas atlas = assets.get("packed/pack.atlas");
+		final String prefix = "game/sprites/" + textureName;
+		final Animation<TextureRegion> runAnimation = new Animation<TextureRegion>(.15f,
+				atlas.findRegions(prefix + "_run_anim"), Animation.PlayMode.LOOP);
+		final Animation<TextureRegion> hitAnimation = new Animation<TextureRegion>(.5f,
+				atlas.findRegions(prefix + "_hit_anim"), Animation.PlayMode.NORMAL);
+		final Animation<TextureRegion> idleAnimation = new Animation<TextureRegion>(.25f,
+				atlas.findRegions(prefix + "_idle_anim"), Animation.PlayMode.LOOP);
+		addAnimation("idle", idleAnimation);
+		addAnimation("run", runAnimation);
+		addAnimation("hit", hitAnimation);
 
-    }
+	}
 
-    public void draw(SpriteBatch batch) {
-        Vector2 pos = this.getPos();
-        TextureRegion frame = this.getFrame();
-        // gets width each frame as it can change
-        batch.draw(frame, pos.x - width / 2, pos.y - height / 2, frame.getRegionWidth() / 16.f, frame.getRegionHeight() / 16.f); /* had to add coord offsets to account for being at center of object + weird height/8 offset accounts for extra whitespace in texture   */
-    }
+	public void draw(SpriteBatch batch) {
+		final Vector2 pos = getPos();
+		final TextureRegion frame = getFrame();
+		// gets width each frame as it can change
+		batch.draw(frame, pos.x - width / 2, pos.y - height / 2, frame.getRegionWidth() / 16.f,
+				frame.getRegionHeight() / 16.f); /*
+													 * had to add coord offsets to account for being at center of object
+													 * + weird height/8 offset accounts for extra whitespace in texture
+													 */
+	}
 
-    public Vector2 getPos() {
-        return body.getPosition();
-    }
+	public Vector2 getPos() {
+		return body.getPosition();
+	}
 
-    public void updateFlippage() {
-        if (movement.x < 0) {
-            this.isXFlipped = true;
-        } else if (movement.x > 0) {
-            this.isXFlipped = false;
-        }
-    }
+	public void updateFlippage() {
+		if (movement.x < 0) {
+			isXFlipped = true;
+		} else if (movement.x > 0) {
+			isXFlipped = false;
+		}
+	}
 
-    public void getHit() {
-        playAnimation("hit");
-    }
+	public void getHit() {
+		playAnimation("hit");
+	}
 
-    @Override
-    public void update(float dt) {
-        super.update(dt);
-        if (currentAnimation.equals("hit") && getAnimation().isAnimationFinished(animTime)) {
-            if (isRunning) {
-                playAnimation("run");
-            } else {
-                playAnimation("idle");
-            }
-        }
+	@Override
+	public void update(float dt) {
+		super.update(dt);
+		if (currentAnimation.equals("hit") && getAnimation().isAnimationFinished(animTime)) {
+			if (isRunning) {
+				playAnimation("run");
+			} else {
+				playAnimation("idle");
+			}
+		}
 //        body.setLinearVelocity(movement.x * speed, movement.y * speed);
-        Vector2 mov = body.getLinearVelocity();
-        body.setLinearVelocity(movement.x != 0 ? movement.x * speed : mov.x, movement.y != 0 ? movement.y * speed : mov.y);
-        if (!this.isRunning && (body.getLinearVelocity().x > 0.1 || body.getLinearVelocity().x < -0.1)) {
-            this.isRunning = true;
-            this.playAnimation("run");
-            System.out.println("run");
-        } else if (this.isRunning && (body.getLinearVelocity().x < 0.2 && body.getLinearVelocity().x > -0.2)) {
-            this.isRunning = false;
-            this.playAnimation("idle");
-        }
-    }
+		final Vector2 mov = body.getLinearVelocity();
+		body.setLinearVelocity(movement.x != 0 ? movement.x * speed : mov.x,
+				movement.y != 0 ? movement.y * speed : mov.y);
+		if (!isRunning && (body.getLinearVelocity().x > 0.1 || body.getLinearVelocity().x < -0.1)) {
+			isRunning = true;
+			playAnimation("run");
+			System.out.println("run");
+		} else if (isRunning && body.getLinearVelocity().x < 0.2 && body.getLinearVelocity().x > -0.2) {
+			isRunning = false;
+			playAnimation("idle");
+		}
+	}
 
-    @Override
-    public void beginCollide(Fixture fixture) {
+	@Override
+	public void beginCollide(Fixture fixture) {
 
-    }
+	}
 
-    @Override
-    public void endCollide(Fixture fixture) {
+	@Override
+	public void endCollide(Fixture fixture) {
 
-    }
-
+	}
 
 }
